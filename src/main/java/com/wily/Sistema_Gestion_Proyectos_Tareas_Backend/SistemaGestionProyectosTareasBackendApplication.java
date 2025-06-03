9e3e71cba440597118b4a07b7328fe7eed074eac
W11LY <mwilliam1412@gmail.com>
2025-06-02 21:43:22 -0500

    init project

---
+++ src/main/java/com/wily/Sistema_Gestion_Proyectos_Tareas_Backend/SistemaGestionProyectosTareasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wily.Sistema_Gestion_Proyectos_Tareas_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestionProyectosTareasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestionProyectosTareasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wily/Sistema_Gestion_Proyectos_Tareas_Backend/SistemaGestionProyectosTareasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wily.Sistema_Gestion_Proyectos_Tareas_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestionProyectosTareasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
